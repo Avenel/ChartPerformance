ce21e3aba5c577f0126a1fa3668371f568c04a3d
Avenel <martin.briewig@googlemail.com>
2014-12-01 18:40:45 +0100

    initial commit

---
+++ ChartPerformancetester/Pages/ChartPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using ChartPerformancetester.Helpers;
+
+
+namespace ChartPerformancetester
+{
+	public class ChartPage : NavigationPage
+	{
+		public ChartPage ()
+		{
+			Tint = Helpers.Color.Blue.ToFormsColor ();
+			Title = "Chart Performance";
+		
+		}
+	}
+}
+
